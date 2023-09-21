968efa39d272704d2230fcd55e87131cc83d8626
dudeistroy <tshangela21@gmail.com>
2023-09-21 12:08:31 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,50 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
+#define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a string is composed of digits
- * @str: the string to check
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 1 if the string of composed of digits, 0 otherwise
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *str)
+int is_digit(char *s)
 {
-	while (*str)
+	int i = 0;
+
+	while (s[i])
 	{
-		if (*str < '0' || *str > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		str++;
+		i++;
 	}
 	return (1);
 }
 
 /**
+ * _strlen  - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - multiplies two positive numbers
  * @argc: The number of command-line arguments
- * @argv: an array of command-line arguments
+ * @argv: Array of arguments
  *
- * Return: 0 on success, 98 on error
+ * Return: Always 0 (Sucess)
  */
 int main(int argc, char *argv[])
 {
-	unsigned long int num1;
-	unsigned long int num2;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("Error\n");
-		return (98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	num1 = strtoul(argv[1], NULL, 10);
-	num2 = strtoul(argv[2], NULL, 10);
-
-	printf("%lu\n", num1 * num2);
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
