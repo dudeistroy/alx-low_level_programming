788b590bb5a068bcb1ca87213a35e567b1184af5
dudeistroy <tshangela21@gmail.com>
2023-09-21 08:25:29 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,11 +39,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	new_ptr = malloc(new_size);
-
 	if (new_ptr == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < old_size; i++)
 	{
