a04cbaa4116f2d29bd8218864091b140a72a17fb
dudeistroy <tshangela21@gmail.com>
2023-09-26 03:36:39 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - Defines a dog's attributes
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
